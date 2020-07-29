4ee1c8ded638a9ce4d57b25d71f868b3dfb9e13a
Robin Linden <dev@robinlinden.eu>
2020-07-29 19:46:46 +0200

    Fill the window with random pixels

--- main.c
+++ main.c
@@ -2,8 +2,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 480
 
 int main(int argc, char *argv[]) {
+    srand(time(NULL));
+
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
         fprintf(stderr, "Failed to initialize SDL.\n");
         return 1;
@@ -13,7 +19,7 @@ int main(int argc, char *argv[]) {
     SDL_Window *const window = SDL_CreateWindow(
             "Keter",
             SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-            640, 480,
+            WINDOW_WIDTH, WINDOW_HEIGHT,
             SDL_WINDOW_SHOWN);
     if (window == NULL) {
         fprintf(stderr, "Failed to create SDL window.\n");
@@ -31,7 +37,47 @@ int main(int argc, char *argv[]) {
     SDL_RenderClear(renderer);
     SDL_RenderPresent(renderer);
 
+    SDL_Texture *const texture = SDL_CreateTexture(
+            renderer,
+            SDL_PIXELFORMAT_ARGB8888,
+            SDL_TEXTUREACCESS_STREAMING,
+            WINDOW_WIDTH, WINDOW_HEIGHT);
+    if (texture == NULL) {
+        fprintf(stderr, "Failed to create SDL texture.\n");
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        return 1;
+    }
+
+    SDL_Delay(1000);
+
+    uint32_t *const pixels = calloc(1, WINDOW_WIDTH * WINDOW_HEIGHT * sizeof(uint32_t));
+    if (pixels == NULL) {
+        fprintf(stderr, "Failed to allocate for pixel data.\n");
+        SDL_DestroyTexture(texture);
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        return 1;
+    }
+
+    for (uint32_t i = 30; i < WINDOW_HEIGHT - 30; ++i) {
+        for (uint32_t j = 30; j < WINDOW_WIDTH - 30; ++j) {
+            uint32_t pixel = rand() % 255;
+            pixel += rand() % 255 << 8;
+            pixel += rand() % 255 << 16;
+            pixel += rand() % 255 << 24;
+            pixels[i * WINDOW_WIDTH + j] = pixel;
+        }
+    }
+
+    SDL_UpdateTexture(texture, NULL, pixels, WINDOW_WIDTH * sizeof(uint32_t));
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, texture, NULL, NULL);
+    SDL_RenderPresent(renderer);
+
     SDL_Delay(2000);
+    free(pixels);
+    SDL_DestroyTexture(texture);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     return 0;
