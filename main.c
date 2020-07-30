61be01a213c40a10312d10f478656fdcec02ffde
Robin Linden <dev@robinlinden.eu>
2020-07-30 13:47:26 +0200

    Add draw helpers

--- main.c
+++ main.c
@@ -15,26 +15,64 @@
 
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
+#define CLAMP(a, min, max) MIN(MAX((a), (min)), (max))
 
 typedef struct Player {
     float x, y;
     float heading;
 } Player;
 
-static void vertical_line(
+static inline void draw_pixel(uint32_t *const surface, int32_t x, int32_t y, uint32_t color) {
+    surface[CLAMP(y, 0, VIEWPORT_HEIGHT - 1) * VIEWPORT_WIDTH + CLAMP(x, 0, VIEWPORT_WIDTH - 1)] = color;
+}
+
+static void draw_line(
+        uint32_t *const surface,
+        int32_t x0, int32_t y0,
+        int32_t x1, int32_t y1,
+        int32_t color) {
+    x0 = CLAMP(x0, 0, VIEWPORT_WIDTH - 1);
+    x1 = CLAMP(x1, 0, VIEWPORT_WIDTH - 1);
+    y0 = CLAMP(y0, 0, VIEWPORT_HEIGHT - 1);
+    y1 = CLAMP(y1, 0, VIEWPORT_HEIGHT - 1);
+    const int32_t x_min = MIN(x0, x1);
+    const int32_t x_max = MAX(x0, x1);
+    const int32_t y_min = MIN(y0, y1);
+    const int32_t y_max = MAX(y0, y1);
+
+    float dx = x_max - x_min;
+    float dy = y_max - y_min;
+    const float step = MAX(dx, dy);
+    dx /= step;
+    dy /= step;
+
+    float x = x_min;
+    float y = y_min;
+
+    // Fill.
+    for (uint32_t i = 0; i < step; ++i, x += dx, y += dy) {
+        draw_pixel(surface, x, y, color);
+    }
+
+    // Border.
+    draw_pixel(surface, x0, y0, 0);
+    draw_pixel(surface, x1, y1, 0);
+}
+
+static void draw_vertical_line(
         uint32_t *const surface,
         uint32_t x,
         uint32_t y1, uint32_t y2,
         uint32_t color) {
     // Border.
-    surface[y1 * VIEWPORT_WIDTH + x] = 0;
-    surface[y2 * VIEWPORT_WIDTH + x] = 0;
+    draw_pixel(surface, x, y1, 0);
+    draw_pixel(surface, x, y2, 0);
 
     // Fill.
     const uint32_t y_min = MIN(y1, y2);
     const uint32_t y_max = MAX(y1, y2);
     for (uint32_t y = y_min + 1; y < y_max; ++y) {
-        surface[y * VIEWPORT_WIDTH + x] = color;
+        draw_pixel(surface, x, y, color);
     }
 }
 
@@ -43,13 +81,13 @@ static void draw_player(
         const Player *const p) {
     for (uint32_t y = p->y - 5; y < p->y + 5; ++y) {
         for (uint32_t x = p->x - 5; x < p->x + 5; ++x) {
-            surface[y * VIEWPORT_WIDTH + x] = 0x00FF0000;
+            draw_pixel(surface, x, y, 0x00FF0000);
         }
     }
 
     const uint32_t heading_y = p->y + sinf(p->heading) * 15;
     const uint32_t heading_x = p->x + cosf(p->heading) * 15;
-    surface[heading_y * VIEWPORT_WIDTH + heading_x] = 0x00FF00FF;
+    draw_pixel(surface, heading_x, heading_y, 0x00FF00FF);
 }
 
 int main(int argc, char *argv[]) {
@@ -129,11 +167,13 @@ int main(int argc, char *argv[]) {
     bool running = true;
     while (running) {
         memset(pixels0, 0, VIEWPORT_WIDTH * VIEWPORT_HEIGHT * sizeof(uint32_t));
-        memset(pixels1, 0xCD, VIEWPORT_WIDTH * VIEWPORT_HEIGHT * sizeof(uint32_t));
-        vertical_line(pixels1, 100, 30, VIEWPORT_HEIGHT - 30, 0x00FF00FF);
         draw_player(pixels0, &p);
-        vertical_line(pixels0, 300, 100, 400, 0x0000FFFF);
+        draw_vertical_line(pixels0, 300, 100, 400, 0x0000FFFF);
         SDL_UpdateTexture(texture, &viewport0, pixels0, VIEWPORT_WIDTH * sizeof(uint32_t));
+
+        memset(pixels1, 0xCD, VIEWPORT_WIDTH * VIEWPORT_HEIGHT * sizeof(uint32_t));
+        draw_vertical_line(pixels1, 100, 30, VIEWPORT_HEIGHT - 30, 0x00FF00FF);
+        draw_line(pixels1, 10, 10, 100, 200, 0x00FF00FF);
         SDL_UpdateTexture(texture, &viewport1, pixels1, VIEWPORT_WIDTH * sizeof(uint32_t));
         SDL_RenderClear(renderer);
         SDL_RenderCopy(renderer, texture, NULL, NULL);
