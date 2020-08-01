d64c22ac2d88ad6848aff8ca43590ba270f22492
Robin Linden <dev@robinlinden.eu>
2020-08-01 18:46:37 +0200

    Correctly draw objects partially behind the player
    Cleanup needed, but it works.

--- main.c
+++ main.c
@@ -23,6 +23,26 @@ typedef struct Player {
     float heading;
 } Player;
 
+static inline float cross(float x0, float y0, float x1, float y1) {
+    return x0 * y1 - y0 * x1;
+}
+
+static inline bool intersect(
+        float *x, float *y,
+        float x0, float y0, float x1, float y1,
+        float x2, float y2, float x3, float y3) {
+    const float a = cross(x0, y0, x1, y1);
+    const float b = cross(x2, y2, x3, y3);
+    const float d = cross(x0 - x1, y0 - y1, x2 - x3, y2 - y3);
+    if (d == 0) {
+        return false;
+    }
+
+    *x = cross(a, x0 - x1, b, x2 - x3) / d;
+    *y = cross(a, y0 - y1, b, y2 - y3) / d;
+    return true;
+}
+
 static inline void draw_pixel(uint32_t *const surface, int32_t x, int32_t y, uint32_t color) {
     if (y < 0 || y > VIEWPORT_HEIGHT - 1) return;
     if (x < 0 || x > VIEWPORT_WIDTH - 1) return;
@@ -130,8 +150,8 @@ static void draw_wall(
     make_relative(p, &x0, &y0);
     make_relative(p, &x1, &y1);
 
-    const float z0 = -y0;
-    const float z1 = -y1;
+    float z0 = -y0;
+    float z1 = -y1;
 
     // Is the object fully behind the player?
     if (z0 <= 0 && z1 <= 0) {
@@ -140,7 +160,33 @@ static void draw_wall(
 
     // Is the object partially behind the player?
     if (z0 <= 0 || z1 <= 0) {
-        return;
+        const float nearz = 1e-4f, farz = 5, nearx = 1e-5f, farx = 20.f;
+
+        // Find an intersection between the wall and the approximate edges of player's view
+        float lx, ly;
+        float rx, ry;
+        intersect(&lx, &ly, x0, z0, x1, z1, -nearx, nearz, -farx, farz);
+        intersect(&rx, &ry, x0, z0, x1, z1, nearx, nearz, farx, farz);
+
+        if (z0 < nearz) {
+            if (ly > 0) {
+                x0 = lx;
+                y0 = z0 = ly;
+            } else {
+                x0 = rx;
+                y0 = z0 = ry;
+            }
+            y0 = -y0;
+        } else if (z1 < nearz) {
+            if (ly > 0) {
+                x1 = lx;
+                y1 = z1 = ly;
+            } else {
+                x1 = rx;
+                y1 = z1 = ry;
+            }
+            y1 = -y1;
+        }
     }
 
     x0 = -x0 * 128 / y0;
